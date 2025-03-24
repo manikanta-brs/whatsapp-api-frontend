2bea4c03a1b20859000cfe5f96c2a8ad1cd52e46
Manikanta Dongala <maniknatadon0000@gmail.com>
2025-03-24 19:21:17 +0530

    third test commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // Allows access from network
+    port: 5173, // Default Vite port, change if needed
+    strictPort: true, // Ensures it doesn't switch to a different port
+  },
 });
