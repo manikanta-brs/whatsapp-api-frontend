d0c99f938d47a5a991a979526fa11b7b100b209c
Manikanta Dongala <maniknatadon0000@gmail.com>
2025-03-24 19:36:49 +0530

    third test commit

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: "0.0.0.0", // Allows access from network
-    port: 5173, // Default Vite port, change if needed
-    strictPort: true, // Ensures it doesn't switch to a different port
+    host: "0.0.0.0", // Allow external access
+    port: 5173, // Ensure correct port
+    strictPort: true, // Prevents Vite from switching ports
+    allowedHosts: ["whatsapp-api-frontend.onrender.com"], // Add your Render domain here
   },
 });
